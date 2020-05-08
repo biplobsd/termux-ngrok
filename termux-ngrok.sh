6355110dbdaf74fce401ae0c0bd4a16365e6e4a9
TermuxGuide <64996709+TermuxGuide@users.noreply.github.com>
2020-05-09 04:16:00 +0600

    Add files via upload

---
+++ termux-ngrok.sh
@@ -0,0 +1,64 @@
+#!/bin/dash
+# Desenvolvido por Tchelo Noob
+atualizar='\033[1;32m'
+echo "$Идёт установка..."
+apt update && upgrade -y
+apt install -y wget
+clear
+k='\033[01;32m'
+echo
+echo "${k}████████╗ ██████╗██╗  ██╗███████╗██╗      ██████╗ ";
+echo "${k}╚══██╔══╝██╔════╝██║  ██║██╔════╝██║     ██╔═══██╗";
+echo "${k}   ██║   ██║     ███████║█████╗  ██║     ██║   ██║";
+echo "${k}   ██║   ██║     ██╔══██║██╔══╝  ██║     ██║   ██║";
+echo "${k}   ██║   ╚██████╗██║  ██║███████╗███████╗╚██████╔╝";
+echo "${k}   ╚═╝    ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝ ";
+echo "${k} /_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/";
+echo
+echo "Установить Ngrok? [Y/n]"
+read opcao
+case $opcao in
+y)
+echo
+echo "Установка Termux-ngrok..."
+case `dpkg --print-architecture` in
+aarch64)
+    architectureURL="arm64" ;;
+arm)
+    architectureURL="arm" ;;
+armhf)
+    architectureURL="armhf" ;;
+amd64)
+    architectureURL="amd64" ;;
+i*86)
+    architectureURL="i386" ;;
+x86_64)
+    architectureURL="amd64" ;;
+*)
+    echo "Архитектура неизвестна"
+esac
+
+wget "https://github.com/tchelospy/NgrokTest/blob/master/ngrok-stable-linux-${architectureURL}.zip?raw=true" -O ngrok.zip
+unzip ngrok.zip
+cat ngrok > /data/data/com.termux/files/usr/bin/ngrok
+chmod 700 /data/data/com.termux/files/usr/bin/ngrok
+rm ngrok ngrok.zip
+clear
+echo "${k}███╗   ██╗ ██████╗ ██████╗  ██████╗ ██╗  ██╗";
+echo "${k}████╗  ██║██╔════╝ ██╔══██╗██╔═══██╗██║ ██╔╝";
+echo "${k}██╔██╗ ██║██║  ███╗██████╔╝██║   ██║█████╔╝ ";
+echo "${k}██║╚██╗██║██║   ██║██╔══██╗██║   ██║██╔═██╗ ";
+echo "${k}██║ ╚████║╚██████╔╝██║  ██║╚██████╔╝██║  ██╗";
+echo "${k}╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝";
+echo
+echo "Напишите (ngrok http (порт) для запуска"
+;;
+
+n)
+clear
+echo "Ngrok не установлен :("
+echo
+esac
+
+
+
